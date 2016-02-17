16e2538d842514ae13ba5aa419c81f9b008debf2
Hakimu Jackson <hakimu@newrelic.com>
2016-02-17 09:33:31 -0800

    Updated to use mongo gem version 2.1.2

--- app.rb
+++ app.rb
@@ -6,16 +6,20 @@ require 'newrelic_rpm'
 
 include Mongo
 
-db = MongoClient.new("localhost", 27017).db("new_mydb")
-coll = db.collection("testCollection")
+# I was using MongoClient.new for previous mongo version. Revised to Mongo::Client.new
+#db = MongoClient.new("localhost", 27017).db("new_mydb")
+# coll = db.collection("testCollection")
+db = Mongo::Client.new([ '127.0.0.1:27017' ], :database => 'new_mydb')
 
 get '/' do
-	@items = coll.find()
+	#@items = coll.find()
+	@items = db[:words].find()
 	erb :home	
 end
 
 get '/:word'do
-  word = coll.insert({item: params[:word]})
+  # word = coll.insert({item: params[:word]})
+  word = db[:words].insert_one({item: params[:word]})
   redirect '/'
 end
 
